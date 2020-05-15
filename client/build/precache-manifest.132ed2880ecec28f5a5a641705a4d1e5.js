903aeb71abf1b1e2b2d1ae3ead6b68e2da596589
Datafluisteraar <steven.gort@ictu.nl>
2020-05-15 21:36:16 +0200

    fixed homepage reference

---
+++ client/build/precache-manifest.132ed2880ecec28f5a5a641705a4d1e5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab88752394987d8c342b7bcdb1f34d90",
+    "url": "/bvv/index.html"
+  },
+  {
+    "revision": "8cd74af4a2835446e8b2",
+    "url": "/bvv/static/css/2.dc67f1aa.chunk.css"
+  },
+  {
+    "revision": "10e2671919cc3a7f1fe0",
+    "url": "/bvv/static/css/main.ba61d6ae.chunk.css"
+  },
+  {
+    "revision": "8cd74af4a2835446e8b2",
+    "url": "/bvv/static/js/2.fc996d97.chunk.js"
+  },
+  {
+    "revision": "cd2e27b8f81904c1e1e1d46c36003baf",
+    "url": "/bvv/static/js/2.fc996d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e2671919cc3a7f1fe0",
+    "url": "/bvv/static/js/main.8568bf3f.chunk.js"
+  },
+  {
+    "revision": "b7ed485ec96ad09a5f8b",
+    "url": "/bvv/static/js/runtime-main.5a31e086.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/bvv/static/media/brand-icons.088a34f7.eot"
+  },
+  {
+    "revision": "0eb8cedcedefe0b04a200dc5c9602f4a",
+    "url": "/bvv/static/media/brand-icons.0eb8cedc.svg"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/bvv/static/media/brand-icons.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/bvv/static/media/brand-icons.822d94f1.woff2"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/bvv/static/media/brand-icons.f4920c94.woff"
+  },
+  {
+    "revision": "2eeb91e4e6722f5bdd934a56580392a2",
+    "url": "/bvv/static/media/discipl-api-strategy.2eeb91e4.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/bvv/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/bvv/static/media/icons.2aa6edf8.ttf"
+  },
+  {
+    "revision": "3719793fcebc92ff6642bf287db11b16",
+    "url": "/bvv/static/media/icons.3719793f.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/bvv/static/media/icons.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/bvv/static/media/icons.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/bvv/static/media/icons.f6121be5.woff2"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/bvv/static/media/outline-icons.3ac49cb3.eot"
+  },
+  {
+    "revision": "9b4c53aa538b8e0c4def479a43a8acf1",
+    "url": "/bvv/static/media/outline-icons.9b4c53aa.svg"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/bvv/static/media/outline-icons.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/bvv/static/media/outline-icons.a57bcf76.woff"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/bvv/static/media/outline-icons.ece54318.ttf"
+  },
+  {
+    "revision": "4ae319a4eedeec8cc063e9aba4ead8b0",
+    "url": "/bvv/static/media/profile1.4ae319a4.jpg"
+  }
+]);
